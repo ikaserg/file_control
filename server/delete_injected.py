62bc4e676e02245cca6b999f1f8803bb602fafec
Sergey Karpukhin <ikaserg@gmail.com>
2020-01-27 00:19:48 +0300

    5

--- server/delete_injected.py
+++ server/delete_injected.py
@@ -5,7 +5,7 @@ import numpy as np
 
 get_diff_url = "http://studio-tadema.net/control_files.php?action=diff"
 file_list = json.loads(urllib.request.urlopen(get_diff_url).read())
-chunk_size = 20
+chunk_size = 25
 for files_chunk in np.array_split(np.array(file_list), chunk_size):
     r = requests.post("http://studio-tadema.net/control_files.php?action=delete_injected", \
                     json={'delete':[{'rel_path':f['rel_path']} for f in files_chunk if f['status'] == 3]})

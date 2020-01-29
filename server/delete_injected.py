50b90f4f8bdd45a6e1fdda971e738fa52ca54d5f
Sergey Karpukhin <ikaserg@gmail.com>
2020-01-30 00:15:47 +0300

    print result

--- server/delete_injected.py
+++ server/delete_injected.py
@@ -9,17 +9,17 @@ def clear_site(fc_url):
     cnt = 0
     while (True):
         file_list = json.loads(urllib.request.urlopen(get_diff_url).read())
-        print(fc_url + "Diff size " + str(len(file_list)))
+        print(fc_url + ": diff size " + str(len(file_list)))
         delete_list = [{'rel_path':f['rel_path']} for f in file_list if f['status'] == 3]
         if (len(delete_list) > 0):
             r = requests.post(fc_url + "?action=delete", \
                             json={'delete':delete_list})
             cnt = len(json.loads(r.text))
-            print(fc_url + "delete " + str(cnt))
+            print(fc_url + ": deleted " + str(cnt))
         restore_list = [{'rel_path':f['rel_path']} for f in file_list if (f['status'] == 2) and (f['rel_path'] != './control_files.php')]
         if (len(restore_list) > 0):
             r = requests.post(fc_url + "?action=restore", \
-                            json={'restore': restore_list})
+                            json={': restored': restore_list})
             cnt += len(json.loads(r.text)) 
             print(fc_url + "restore " + str(cnt))
 

03ae3ed6509363835c070620ca9d5dbbfa033959
Sergey Karpukhin <ikaserg@gmail.com>
2020-01-30 00:07:03 +0300

    add autoschool

--- server/delete_injected.py
+++ server/delete_injected.py
@@ -3,23 +3,26 @@ import urllib
 import json
 import numpy as np
 
-chunk_size = 25
-get_diff_url = "http://studio-tadema.net/control_files.php?action=diff&limit="+str(chunk_size)
-cnt = 0
-while (True):
-    file_list = json.loads(urllib.request.urlopen(get_diff_url).read())
-    delete_list = [{'rel_path':f['rel_path']} for f in file_list if f['status'] == 3]
-    if (len(delete_list) > 0):
-        r = requests.post("http://studio-tadema.net/control_files.php?action=delete", \
-                        json={'delete':delete_list})
-        cnt = len(json.loads(r.text))
-    restore_list = [{'rel_path':f['rel_path']} for f in file_list if (f['status'] == 2) and (f['rel_path'] != './control_files.php')]
-    if (len(restore_list) > 0):
-        r = requests.post("http://studio-tadema.net/control_files.php?action=restore", \
-                        json={'restore': restore_list})
-        cnt += len(json.loads(r.text)) 
-    if (cnt == 0):
-        break
+def clear_site(fc_url):
+    chunk_size = 25
+    get_diff_url = fc_url + "?action=diff&limit="+str(chunk_size)
+    cnt = 0
+    while (True):
+        file_list = json.loads(urllib.request.urlopen(get_diff_url).read())
+        delete_list = [{'rel_path':f['rel_path']} for f in file_list if f['status'] == 3]
+        if (len(delete_list) > 0):
+            r = requests.post(fc_url + "?action=delete", \
+                            json={'delete':delete_list})
+            cnt = len(json.loads(r.text))
+        restore_list = [{'rel_path':f['rel_path']} for f in file_list if (f['status'] == 2) and (f['rel_path'] != './control_files.php')]
+        if (len(restore_list) > 0):
+            r = requests.post(fc_url + "?action=restore", \
+                            json={'restore': restore_list})
+            cnt += len(json.loads(r.text)) 
+        if (cnt == 0):
+            break
+clear_site("http://studio-tadema.net/control_files.php")        
+clear_site("https://rzn438043.ru/control_files.php")        
 
 #    for files_chunk in np.array_split(np.array(file_list), chunk_size):
 #        r = requests.post("http://studio-tadema.net/control_files.php?action=delete_injected", \

3dfb7de0c00b7a57fe2f635ccee69bb8ef6dad49
Sergey Karpukhin <ikaserg@gmail.com>
2020-02-02 23:24:52 +0300

    https

--- server/delete_injected.py
+++ server/delete_injected.py
@@ -26,12 +26,12 @@ def clear_site(fc_url, depth):
 
         if (cnt == 0):
             break
-clear_site("http://studio-tadema.net/control_files.php", 0)        
-clear_site("http://studio-tadema.net/control_files.php", 10000)        
-clear_site("http://rzn438043.ru/control_files.php", 0)        
-clear_site("http://rzn438043.ru/control_files.php", 1000)        
-clear_site("http://7111953.ru/control_files.php", 0)        
-clear_site("http://7111953.ru/control_files.php", 1000)        
+clear_site("https://studio-tadema.net/control_files.php", 0)        
+clear_site("https://studio-tadema.net/control_files.php", 10000)        
+clear_site("https://rzn438043.ru/control_files.php", 0)        
+clear_site("https://rzn438043.ru/control_files.php", 1000)        
+clear_site("https://7111953.ru/control_files.php", 0)        
+clear_site("https://7111953.ru/control_files.php", 1000)        
 
 #    for files_chunk in np.array_split(np.array(file_list), chunk_size):
 #        r = requests.post("http://studio-tadema.net/control_files.php?action=delete_injected", \
